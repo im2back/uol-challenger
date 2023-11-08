49d87b6e60d1b2b5849e40b10b1af2fe7fb418fe
Jefferson Richards <jefferson.richards@outlook.com>
2023-11-08 08:54:10 -0300

    Refatoramento da classe PlayerService

--- src/main/java/im2back/com/github/uolchallenger/service/CodinomesInUseService.java
+++ src/main/java/im2back/com/github/uolchallenger/service/CodinomesInUseService.java
@@ -1,8 +1,13 @@
 package im2back.com.github.uolchallenger.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import im2back.com.github.uolchallenger.infra.exceptions.ServiceExeptions;
 import im2back.com.github.uolchallenger.model.codinomes.CodinomesInUse;
 import im2back.com.github.uolchallenger.repositories.CodinomesInUseRepository;
 
@@ -12,8 +17,49 @@ public class CodinomesInUseService {
 	@Autowired
 	private CodinomesInUseRepository repository;
 	
+	@Autowired
+	private XmlService xmlservice;
+	
+	@Autowired
+	private JsonService jsonservice;
+	
 	public void saveCodinome(String codinomeParam) {
 		CodinomesInUse codinome = new CodinomesInUse(codinomeParam);
 		repository.save(codinome);
 	}
+	
+	public List<String> findall(){
+		List<CodinomesInUse> list = repository.findAll();
+		List<String> codinomesInUseList = new ArrayList<>();
+		
+		for(CodinomesInUse element: list) {
+			codinomesInUseList.add(element.getCodinome());
+		}	
+		
+		return codinomesInUseList;
+	}
+	
+	public String sortearCodinome(String grupo) {
+		
+		List<String> listaCodinomes = new ArrayList<>();
+		  if(grupo.equals("Vingadores")){
+			listaCodinomes = jsonservice.fetchJsonData();
+		} if(grupo.equals("Liga da Justiça")) {
+			listaCodinomes = xmlservice.getCodinomesDaLigaDaJustica();
+		}
+					
+		List<String> listaCodinomesEmUso = findall();
+		
+		listaCodinomes.removeAll(listaCodinomesEmUso);
+
+		if (listaCodinomes.isEmpty()) {
+			throw new ServiceExeptions("Não existem mais codinomes disponiveis, por favor selecione outro grupo");
+		}
+		
+		Random random = new Random();
+		int indiceSorteado = random.nextInt(listaCodinomes.size());
+		return listaCodinomes.get(indiceSorteado);
+	}
+	
+	
 }
--- src/main/java/im2back/com/github/uolchallenger/service/PlayerService.java
+++ src/main/java/im2back/com/github/uolchallenger/service/PlayerService.java
@@ -1,8 +1,6 @@
 package im2back.com.github.uolchallenger.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,52 +17,28 @@ public class PlayerService {
 	private PlayerRepository repository;
 
 	@Autowired
-	private JsonService jsonservice;
-	
-	@Autowired
-	private XmlService xmlservice;
-
-
-	private static List<String> vingadoresCodinomesemUso = new ArrayList<>();
-	private static List<String> ligaCodinomesemUso = new ArrayList<>();
+	private CodinomesInUseService codinomeService;
 
 	public List<Player> findAll() {
 		return repository.findAll();
 	}
 
 	public PlayerResponseDTO insertNewPlayerAvenger(PlayerRequestDTO playerDTO, String grupo) {
-		
-		if(grupo.equals("Vingadores")) {
-				List<String> avengerCondinomesList = jsonservice.fetchJsonData();
-				String codinomeLivre = sortearCodinome(avengerCondinomesList,vingadoresCodinomesemUso);
-				Player player = new Player(playerDTO, codinomeLivre, grupo);
-				vingadoresCodinomesemUso.add(player.getCodinome());
-				repository.save(player);
-				var response = new PlayerResponseDTO(player);
-				return response;
-			}
-			if(grupo.equals("liga-da-justiça")) {
-				List<String> listaCodinomesLiga =xmlservice.getCodinomesDaLigaDaJustica();
-				String codinomeLivre = sortearCodinome(listaCodinomesLiga,ligaCodinomesemUso);
-				Player player = new Player(playerDTO, codinomeLivre, "Liga da Justiça");
-				ligaCodinomesemUso.add(player.getCodinome());
-				repository.save(player);
-				var response = new PlayerResponseDTO(player);
-				return response;
-		} else throw new ServiceExeptions("Selecione um Grupo Válido : Liga da Justiça ou Vingadores.");
-	}
 
-	private String sortearCodinome(List<String> listaCompleta, List<String> listaCodinomesEmUso) {
-		
-		listaCompleta.removeAll(listaCodinomesEmUso);
-
-		if (listaCompleta.isEmpty()) {
-			throw new ServiceExeptions("Não existem mais codinomes disponiveis, por favor selecione outro grupo");
+		if (grupo.equals("Vingadores") == false && grupo.equals("Liga da Justiça") == false) {
+			throw new ServiceExeptions("Selecione um Grupo Válido : Liga da Justiça ou Vingadores.");
 		}
-		
-		Random random = new Random();
-		int indiceSorteado = random.nextInt(listaCompleta.size());
-		return listaCompleta.get(indiceSorteado);
+
+		String codinomeLivre = codinomeService.sortearCodinome(grupo);
+
+		Player player = new Player(playerDTO, codinomeLivre, grupo);
+		repository.save(player);
+
+		codinomeService.saveCodinome(player.getCodinome());
+
+		var response = new PlayerResponseDTO(player);
+		return response;
+
 	}
 
 }
